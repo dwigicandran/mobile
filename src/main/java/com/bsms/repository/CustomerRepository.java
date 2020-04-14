ada8e6b471e625cf0b5431931354843721d5b0ae
admin <admin@localhost>
2020-04-15 06:35:29 +0700

    add native query jpa

--- src/main/java/com/bsms/repository/CustomerRepository.java
+++ src/main/java/com/bsms/repository/CustomerRepository.java
@@ -2,7 +2,10 @@ package com.bsms.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +15,22 @@ import com.bsms.domain.Customer;
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 	
 	Customer findByMsisdn(String msisdn);
+	
 	long countByMsisdn(String msisdn);
+	
+	@Query(value = "select msisdn from Customer where id = :ID")
+	String getDataByID(@Param("ID") Long id);
+	
+	@Query(value = "select failedpincount from Customer where ID = :id")
+	Integer getFailedPINCountById(@Param("id") Long id);
+	
+	@Modifying(clearAutomatically = true)
+	@Query(value = "update Customer set failedpincount = :valpin where msisdn = :msisdn")
+	void updatePINCountById(@Param("valpin") Integer valpin, @Param("msisdn") String msisdn);
+	
+	@Query(value = "SELECT ID, Name, ActivationCode, email, MSISDN, TAK, MACHEX, CreateOTPDate "
+		    		+ "FROM Customer with (NOLOCK) where MSISDN in (:msisdn1,:msisdn2)", nativeQuery = true)
+	Customer getByMsisdn(@Param("msisdn1") String msisdn1, @Param("msisdn2") String msisdn2);
+
 	
 }
