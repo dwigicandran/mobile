f2bebed631c84a107c76a9d22dac567c3151a5ff
admin <admin@localhost>
2020-04-19 07:07:19 +0700

    add OnlineTrfResp, TrfMethod, TrfMethodDispResp

---
+++ src/main/java/com/bsms/restobjclient/OnlineTrfResp.java
@@ -0,0 +1,27 @@
+package com.bsms.restobjclient;
+
+import java.io.Serializable;
+
+import com.bsms.restobj.MbApiContentResp;
+import com.dto.onlineTransfer.Content;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonInclude(Include.NON_NULL)
+@Getter
+@Setter
+public class OnlineTrfResp implements Serializable, MbApiContentResp {
+
+	private static final long serialVersionUID = 4164245547044293391L;
+	
+	private String responseCode;
+	private String correlationId;
+	private String transactionId;
+	private Content content;
+	
+}
---
+++ src/main/java/com/bsms/restobjclient/TrfMethodDispResp.java
@@ -0,0 +1,31 @@
+package com.bsms.restobjclient;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.bsms.restobj.MbApiContentResp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonInclude(Include.NON_NULL)
+@Getter
+@Setter
+public class TrfMethodDispResp implements MbApiContentResp, Serializable {
+	
+	private static final long serialVersionUID = 5074717308798811211L;
+	
+	private List<TrfMethod> content=null;
+	
+	public TrfMethodDispResp(List<TrfMethod> trf) {
+				
+				this.content=trf;
+				
+	}
+	
+}
+
