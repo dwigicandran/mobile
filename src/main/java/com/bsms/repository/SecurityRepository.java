69749ced25c859c1807ea2b505eaf86ecd5364ba
admin <admin@localhost>
2020-04-19 07:03:53 +0700

    fixing query

--- src/main/java/com/bsms/repository/SecurityRepository.java
+++ src/main/java/com/bsms/repository/SecurityRepository.java
@@ -1,6 +1,9 @@
 package com.bsms.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.bsms.domain.Security;
 
@@ -9,5 +12,11 @@ public interface SecurityRepository extends CrudRepository<Security, Long> {
 	Security findByCustomerId(Long customerId);
 	
 	Security findByMbSessionId(String mbSessionId);
+	
+//	Security deleteByCustomerId(long customerId);
+	
+	@Modifying(clearAutomatically = true)
+	@Query(value = "DELETE FROM Security where customerId = :customerId")
+	void deleteByCustId(@Param("customerId") long customerId);
 	
 }
