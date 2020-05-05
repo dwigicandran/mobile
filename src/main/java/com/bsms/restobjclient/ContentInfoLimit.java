0811c719a4e6f1176f4c07bada3e76308e9c3e77
Dodo Prawira Pradana <dodoprawira@yahoo.com>
2020-05-05 05:54:42 +0000

    Update ContentInfoLimit.java

--- src/main/java/com/bsms/restobjclient/ContentInfoLimit.java
+++ src/main/java/com/bsms/restobjclient/ContentInfoLimit.java
@@ -9,13 +9,15 @@ public class ContentInfoLimit {
 	private String key;
 	private String trxAmtLimit;
 	private String dailyAmtLimit;
+	private String limitUsed;
 	private String remainingLimit;
 	
-	public ContentInfoLimit(String key,String trxAmtLimit_display,String dailyAmtLimit_display,String remainingLimit_display) {
+	public ContentInfoLimit(String key,String trxAmtLimit_display,String dailyAmtLimit_display,String limitUsed,String remainingLimit_display) {
 				
 				this.key=key;
 				this.trxAmtLimit=trxAmtLimit_display;
 				this.dailyAmtLimit=dailyAmtLimit_display;
+				this.limitUsed=limitUsed;
 				this.remainingLimit=remainingLimit_display;
 			
 	}
