4e07f9e8dc95e380b51c563cf57b104dafb8e8f2
@dwisutrisno <dwi_sutrisno@live.com>
2020-11-03 16:07:57 +0700

    penambahan constan value dan check limit qurban inquiry

--- src/main/java/com/bsms/cons/MbConstant.java
+++ src/main/java/com/bsms/cons/MbConstant.java
@@ -13,5 +13,22 @@ public class MbConstant {
     
     public static final String SWITCHER_ECHO_TX_ID = "999999";
     public static final String ERROR_SL_GET_TX_ID = "999998";
-	
+
+    public static final String ERROR_REQUEST_ID = "Permintaan tidak dapat diproses, silahkan dicoba beberapa saat lagi.";
+    public static final String ERROR_REQUEST_EN = "Request can't be process, please try again later.";
+
+    public static final String ERROR_LIMIT_EXCEED_EN = "Your transaction has exceeded the limit.";
+    public static final String ERROR_LIMIT_EXCEED_ID = "Transaksi Anda melebihi limit.";
+
+    public static final String ERROR_LIMIT_CHECK_ID = "Check Limit Gagal.";
+    public static final String ERROR_LIMIT_CHECK_EN = "Limit Check Failed.";
+
+    public static final String ERROR_LIMIT_FINANCIAL_ID = "Anda belum bisa melakukan transaksi finansial.\nSilahkan datang ke cabang untuk mengaktifkannya.";
+    public static final String ERROR_LIMIT_FINANCIAL_EN = "You cannot do financial transactions.\nPlease come to our branch to activate it.";
+
+
+
+
+
+
 }
