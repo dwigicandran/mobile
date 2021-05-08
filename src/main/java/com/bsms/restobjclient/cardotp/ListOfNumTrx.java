a0b475df600215e145cb4175ea4007f4422ab47d
dwitya <dwityapk@gmail.com>
2021-05-08 23:14:35 +0700

    add JsonIgnoreProperties annotation

--- src/main/java/com/bsms/restobjclient/cardotp/ListOfNumTrx.java
+++ src/main/java/com/bsms/restobjclient/cardotp/ListOfNumTrx.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import com.bsms.domain.OtpNumTrx;
 import com.bsms.restobj.MbApiContentResp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonInclude(Include.NON_NULL)
 public class ListOfNumTrx implements MbApiContentResp {
 
 	public List<OtpNumTrx> content;
--- src/main/java/com/bsms/restobjclient/cardotp/ListOfPeriodValidity.java
+++ src/main/java/com/bsms/restobjclient/cardotp/ListOfPeriodValidity.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import com.bsms.domain.OtpPeriod;
 import com.bsms.restobj.MbApiContentResp;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonInclude(Include.NON_NULL)
 public class ListOfPeriodValidity implements MbApiContentResp {
 	
 	public List<OtpPeriod> content;
